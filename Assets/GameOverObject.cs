80c8048ec364152b77b054c7e225a16fdc82068a
Alyssa Blanco <alyssakblanco@outlook.com>
2022-11-20 17:05:45 -0700

    assignment complete

---
+++ Assets/GameOverObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverObject : MonoBehaviour {
+
+	// unity accessible text 
+	public Text gameOverTxt;
+
+	// Use this for initialization
+	void Start () {
+		// invisible
+		gameOverTxt.color = new Color(0,0,0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		// visible
+		gameOverTxt.color = new Color(0,0,0,1);
+	}
+}
